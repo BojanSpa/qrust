e07cf5d773b26abca5b3e08291d36a184f704d0f
Bojan Spasevski <bojan.spasevski@outlook.at>
2023-05-28 16:42:59 +0200

    CsvSanitizer

--- src/data/sanitizer.rs
+++ src/data/sanitizer.rs
@@ -1,13 +1,16 @@
+use std::fs::OpenOptions;
 use std::io::Error as IoError;
 use std::io::ErrorKind;
 use std::path::Path;
 
-const HEADER: &str = "open_time,open,high,low,close,volume,close_time,quote_volume,count,taker_buy_volume,taker_buy_quote_volume,ignore";
+use csv::Reader as CsvReader;
+use csv::StringRecord as CsvStringRecord;
+use csv::Writer as CsvWriter;
 
 pub struct CsvSanitizer;
 impl CsvSanitizer {
     pub fn new() -> CsvSanitizer {
-        return CsvSanitizer {};
+        CsvSanitizer {}
     }
 
     pub fn cleanup(&self, csv_path: &Path) -> Result<(), IoError> {
@@ -15,7 +18,30 @@ impl CsvSanitizer {
             return Err(IoError::new(ErrorKind::InvalidInput, "Not a CSV file."));
         }
 
-        return Ok(());
+        let reader = CsvReader::from_path(csv_path)?;
+        if reader.has_headers() {
+            return Ok(());
+        }
+
+        let content_record_results = reader.into_records().collect::<Vec<_>>();
+        if content_record_results.iter().any(|rec| rec.is_err()) {
+            return Err(IoError::new(ErrorKind::InvalidData, "Invalid CSV data."));
+        }
+
+        let mut content_records = content_record_results
+            .into_iter()
+            .map(|rec| rec.unwrap())
+            .collect::<Vec<CsvStringRecord>>();
+        content_records.insert(0, self.get_header_record());
+
+        let csv_file = OpenOptions::new().read(true).write(true).open(csv_path)?;
+        let mut writer = CsvWriter::from_writer(csv_file);
+        for record in content_records {
+            writer.write_record(&record)?;
+        }
+        writer.flush()?;
+
+        Ok(())
     }
 
     fn is_csv(&self, path: &Path) -> bool {
@@ -25,5 +51,18 @@ impl CsvSanitizer {
             Some(ext) => ext == CSV_EXT,
             None => false,
         };
+    }
+
+    fn get_header_record(&self) -> CsvStringRecord {
+        const HEADER: &str = "open_time,open,high,low,close,volume,close_time,quote_volume,count,taker_buy_volume,taker_buy_quote_volume,ignore";
+        let header_parts = HEADER.split(',').collect::<Vec<&str>>();
+        CsvStringRecord::from(header_parts)
+    }
+}
+
+pub struct DataFrameSanitizer;
+impl DataFrameSanitizer {
+    pub fn new() -> DataFrameSanitizer {
+        DataFrameSanitizer {}
     }
 }
